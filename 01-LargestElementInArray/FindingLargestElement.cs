712cf818aadfec03269dd4ce4077f3831495698b
bayangarkov <baiangarkov@gmail.com>
2017-02-15 20:09:45 +0200

    Finding max element from array of ints

---
+++ 01-LargestElementInArray/FindingLargestElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LargestElementInArray
+{
+    class FindingLargestElement
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arrayOfInt = new int[n];
+            for (int i = 0; i < arrayOfInt.Length; i++)
+            {
+                arrayOfInt[i] = int.Parse(Console.ReadLine());
+            }
+
+            int findMax = arrayOfInt.Max();
+
+            Console.WriteLine(findMax);
+        }
+    }
+}
