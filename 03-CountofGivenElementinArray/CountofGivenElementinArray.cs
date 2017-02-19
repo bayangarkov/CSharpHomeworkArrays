b60c7502eefb18490695430f8daab1457b95412e
bayangarkov <baiangarkov@gmail.com>
2017-02-19 10:09:19 +0200

    Finding how many times a element is in the array

---
+++ 03-CountofGivenElementinArray/CountofGivenElementinArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CountofGivenElementinArray
+{
+    class CountofGivenElementinArray
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int chosenNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            foreach (var i in array)
+            {
+                if (i == chosenNumber)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
