14e3af4deb12620b51c017d6002f5d4a6ab9cd72
bayangarkov <baiangarkov@gmail.com>
2017-02-19 12:11:51 +0200

    Checking if all elements are equal

---
+++ 06-EqualSequenceOfElementsInArray/EqualSequenceOfElementsInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_EqualSequenceOfElementsInArray
+{
+    class EqualSequenceOfElementsInArray
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            bool isEqual = true;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] != array[i + 1])
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            if (isEqual == false)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
