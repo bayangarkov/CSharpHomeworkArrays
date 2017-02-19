36108ae06345c431a2fa00f8598f71ead16df56f
bayangarkov <baiangarkov@gmail.com>
2017-02-19 10:16:16 +0200

    Checking how many times we found bigger number than n

---
+++ 04-CountOccurrencesOfLargerNumbersInArray/CountOccurrences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CountOccurrencesOfLargerNumbersInArray
+{
+    class CountOccurrences
+    {
+        static void Main()
+        {
+            double[] array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            double chosenNumber = double.Parse(Console.ReadLine());
+
+            int counter = 0;
+            
+            foreach (var item in array)
+            {
+                if (item > chosenNumber)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
