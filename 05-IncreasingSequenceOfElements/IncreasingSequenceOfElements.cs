fa5e51e0c5e48ce7d326aec965888b5eebae228c
bayangarkov <baiangarkov@gmail.com>
2017-02-19 11:08:46 +0200

    Checking if the next item is larger than the item before

---
+++ 05-IncreasingSequenceOfElements/IncreasingSequenceOfElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_IncreasingSequenceOfElements
+{
+    class IncreasingSequenceOfElements
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            bool isIncrementing = true;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - 1])
+                {
+                    isIncrementing = false;
+                    break;
+                }
+            }
+
+            if (isIncrementing == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
