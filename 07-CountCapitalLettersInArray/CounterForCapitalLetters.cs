687a18735026d3b6e076f374bf2edbf6bc59148e
bayangarkov <baiangarkov@gmail.com>
2017-02-19 12:20:56 +0200

    Count how many times in a string there is a Capital Letter only

---
+++ 07-CountCapitalLettersInArray/CounterForCapitalLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_CountCapitalLettersInArray
+{
+    class CounterForCapitalLetters
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            int counter = 0;
+
+            foreach (var item in words)
+            {
+                if (item.Length == 1)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
