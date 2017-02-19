8fc24576a2e3b5851cce12113fa600fe473f892b
bayangarkov <baiangarkov@gmail.com>
2017-02-19 13:08:04 +0200

    Checking if an array has symmetry

---
+++ 08-ArraySymmetry/CheckingIfArrayIsSymmetry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ArraySymmetry
+{
+    class CheckingIfArrayIsSymmetry
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            string[] words2 = words.ToArray();
+            
+            bool isEqual = words.Reverse().SequenceEqual(words2);
+
+            if (isEqual == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
